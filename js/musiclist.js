871f355f6953f3e874f214494d8997e4c777b8c3
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-06-21 00:12:07 +0530

    second commit

---
+++ js/musiclist.js
@@ -0,0 +1,24 @@
+let allMusic = [
+
+    {
+        name: "The Nights",
+        artist: "Avicii",
+        img: "music1",
+        src: "music1"
+    },
+    {
+        name: "Night Changes",
+        artist: "One Direction",
+        img: "music2",
+        src: "music2"
+    },
+    {
+        name: "Counting Stars",
+        artist: "One Republic",
+        img: "music3",
+        src: "music3"
+    },
+    
+    
+    ];
+    
