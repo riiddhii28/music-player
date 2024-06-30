9c382abc366688e8960f6a4b3e0bb05b57929805
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-06-30 17:30:24 +0530

    latest commit

--- js/script.js
+++ js/script.js
@@ -120,4 +120,121 @@ closemoreMusic.addEventListener("click", () => {
 });
 
 
+ // update progressbar width according to music current time
+mainAudio.addEventListener("timeupdate", (e) => {
+    const currentTime = e.target.currentTime; //getting playing song currenttime
+    const duration = e.target.duration; //getting playing total song duration
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+
+
+
+    let musicCurrentTime = container.querySelector(".current-time"),
+        musicDuration = container.querySelector(".max-duration");
+    mainAudio.addEventListener("loadeddata", () => {
+
+        //  update song total duration
+        let mainAdDuration = mainAudio.duration;
+        let totalMin = Math.floor(mainAdDuration / 60);
+        let totalSec = Math.floor(mainAdDuration % 60);
+        if (totalSec < 10) { //if sec is less than 10 then add 0 before it
+            totalSec = `0${totalSec}`;
+        }
+
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+
+    });
+
+    //update playing song current time
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if (currentSec < 10) { 
+        currentSec = `0${currentSec}`;
+    }
+
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+
+});
+
+// update playing song current width onaccording to the progress bar width
+
+progressArea.addEventListener("click", (e) => {
+    let progressWidth = progressArea.clientWidth; //getting width of progress bar
+    let clickedOffsetX = e.offsetX; //getting offset x value
+    let songDuration = mainAudio.duration; //getting song total duration
+
+    mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+    playMusic();
+
+});
+
+// what to do after song ended
+mainAudio.addEventListener("ended", () => {
+    let getText = repeatBtn.innerText; //getting this tag innerText
+    switch (getText) {
+        case "repeat":
+            nextMusic(); //calling nect music function
+            break;
+        case "repeat_one":
+            mainAudio.currentTime = 0; //setting audio current time to 0
+            loadMusic(musicIndex); //calling load music function with argument, in the argument there is a  index of current song
+            playMusic(); //calling playMusic Function
+            break;
+        case "shuffle":
+            let randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+            do {
+                randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+            } while (musicIndex == randIndex); //thi loop run until the next random number won't be the same of current musicIndex
+            musicIndex = randIndex; //passing randomIndex to musicIndex
+            loadMusic(musicIndex);
+            playMusic();
+            playingSong();
+            break;
+    }
+});
+
+
+
+
+
+// play particular song from the list on click of li tag
+
+const allLiTags = ulTag.querySelectorAll("li");
+function playingSong() {
+    for (let j = 0; j < allLiTags.length; j++) {
+   let audioTag = allLiTags[j].querySelector(".audio-duration");
+        // let remove playing class from all other li expect the last one which is clicked
+        if(allLiTags[j].classList.contains("playing")){
+            allLiTags[j].classList.remove("playing");
+        //  let's get that audio duration value and pass to .audio-duration innertext
+        let adDuration = audioTag.getAttribute("t-duration");
+        audioTag.innerText = adDuration;
+        }
+    
+        // if there is an li tag which li index is equal to musicIndex
+        // then this music is playing now and we'll style it
+    
+        if(allLiTags[j].getAttribute("li-index") == musicIndex){
+            allLiTags[j].classList.add("playing");
+            audioTag.innerText = "Playing";
+        }
+    
+        // adding on click attribute in all li tags
+        allLiTags[j].setAttribute("onclick", "clicked(this)");
+    }
+}
+
+// lets play song on click li 
+function clicked(element){
+
+    // getting li index of particular clicked li tag
+    let getLiIndex = element.getAttribute("li-index");
+    musicIndex =  getLiIndex; //passing that liindex to musicIndex
+    loadMusic(musicIndex);
+    playMusic(); 
+    playingSong();
+   
+}
+
+
 
